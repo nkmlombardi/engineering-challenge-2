2bf8fc6e3f2f0e3ac3a8a87d7d543519744a3ce8
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2016-12-20 11:21:15 -0500

    Success!

--- public/application.js
+++ public/application.js
@@ -58,37 +58,66 @@ function layOutDay(events) {
         return event
     })
 
+
     /*
         Iterate over the array and calculate left and width properties.
      */
     return eventsArray.map(function(event) {
         event.width = 600
         event.maxCollisions = 0
+        event.index = 0
+        event.equal = false
 
         // Determine child event with most collisions
         if (event.collisions.length > 0) {
             event.maxCollisions = Math.max.apply(Math, event.collisions.map(function(o) {
-                return o.collisions.length }))
+                return o.collisions.length
+            }))
+
+            // Determine if all collisions are equivalent
+            event.equal = event.collisions.reduceRight(function(previous, current) {
+                return event.collisions.length === current.collisions.length
+            }, true)
         }
 
-        // Index will default to event with most collisions
-        event.index = event.maxCollisions
+
+        // If event has the same amount of collisions as all other colliding events, then
+        // they should be placed one by one next to each other
+        if (event.equal) {
+            event.collisions.map(function(cEvent, index) {
+                cEvent.index = index
+                cEvent.width = event.width / (event.collisions.length + 1)
+                cEvent.left = cEvent.width * cEvent.index
+            })
+
+            event.width = event.width / (event.collisions.length + 1)
+            event.index = event.collisions.length
+        }
+
 
         // If event with most collisions is more than current event's collisions, set the
         // index to be less so it is aligned to the left of that event
         if (event.collisions.length < event.maxCollisions) {
-            event.index = event.collisions.length - 1
-            event.width = event.width / event.maxCollisions
-        }
+            // event.index = event.collisions.length - 1
+            // event.width = event.width / event.maxCollisions + 1
 
-        // If the event has the most collisions, then use it's collisions to determine the
-        // width of the event
-        if (event.collisions.length > event.maxCollisions) {
-            event.width = event.width / event.collisions.length
+            event.sortedCollisions = event.collisions.sort(function(firstEvent, secondEvent) {
+                return firstEvent.collisions.length < secondEvent.collisions.length
+            })
+
+            event.sortedCollisions.map(function(cEvent, index) {
+                cEvent.index = index + 1
+                cEvent.width = event.width / (event.collisions.length + 1)
+                cEvent.left = cEvent.width * cEvent.index
+            })
+
+            event.width = event.width / (event.maxCollisions)
+            event.index = 0
         }
 
         // Calculate the x position of the element
         event.left = event.width * event.index
+
         return event
     })
 }
@@ -197,14 +226,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     getRequest('https://appcues-interviews.firebaseio.com/calendar/events.json',
         function success(events) {
-            events = JSON.parse(events)
-            events['fake-id'] = {
-                start: 0,
-                end: 60
-            }
-            events['fake-id2'] = {
-                start: 0,
-                end: 60
+            events = {
+                "id-1": { "start": 30, "end": 149 },
+                "id-2": { "start": 580, "end": 649 },
+                "id-3": { "start": 560, "end": 619 },
+                "id-4": { "start": 630, "end": 699 },
+                "id-5": { "start": 580, "end": 649 },
+                "id-6": { "start": 630, "end": 699 },
+                "id-7": { "start": 630, "end": 699 },
+                "id-8": { "start": 580, "end": 649 },
             }
 
             console.log(events)
@@ -224,8 +254,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     'top: ' + event.start + 'px;' +
                     'left: ' + event.left + 'px;'
                 )
-                eventElement.innerHTML = '<span class="title">Sample Item</span>' +
-                    '<p>Sample Location</p>'
+                eventElement.innerHTML = '<span class="title">' + event.id + ' NOC Item</span>' +
+                    '<p>' + event.start + ' -> ' + event.end + '</p>'
                 eventList.appendChild(eventElement)
             })
         },
